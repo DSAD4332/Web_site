11033a53d79d14144834347769961123f209fe90
Tair <tair.aga@icloud.com>
2023-12-20 13:57:16 +0600

    Просто коммит

--- WEB/Web_Site/Web_App/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-from .models import CustomUser
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'address', 'role']
-        widgets = {'password': forms.PasswordInput()}
